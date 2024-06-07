dba11370f616284897a7a3f6ffad9c8802110181
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-07 13:05:20 -0400

    Add html to home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>
+    <a href = 'src/components/Home.js'>Home</a>
+    <a href = 'src/components/About.js'>About</a>
+  </nav>;
 }
 
 export default NavBar;
